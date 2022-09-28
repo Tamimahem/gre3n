68f104b3ac54de1c335d7d46af6a061bbe13bd2a
Tamimahem <114570172+Tamimahem@users.noreply.github.com>
2022-09-28 15:06:26 +0600

    Update fu3k.py

--- fu3k.py
+++ fu3k.py
@@ -1,9 +1,4 @@
-mport os, platform
-print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
-os.system('git pull')
-try:
-
-        import os, sys
+import os, sys
 try:
     __import__("CRACK").Subscraption()
 except Exception as e:
