09f3bc111e38f4b2612df67741db691333d263e5
Tamimahem <114570172+Tamimahem@users.noreply.github.com>
2022-09-28 14:51:37 +0600

    Add files via upload

--- fu3k.py
+++ fu3k.py
@@ -1,18 +1,22 @@
-import os, platform, time
+mport os, platform
 print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
 os.system('git pull')
 try:
-    import gtts
-except IOError:
-    os.system('pip install gtts')
-def Update():
-    exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
-def Run():
-        bit = platform.architecture()[0]
-        if bit == '64bit':
-            print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
 
-            import fuck
-        else:
-            exit('\033[1;31m[×] Device Not Support 32bit')
-Run()
+        import requests
+
+except:
+
+        os.system('pip2 install requests')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == "64bit":
+       print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
+        from fu3k import fuck
+
+        login()
+elif b == '32bit':
+    print("NOT SUPPORTED BRO YOUR PHONE 32 BIT")
