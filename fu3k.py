d2563a9a5549e98c5d1fba8f55f0f48b55a8d863
Tamimahem <114570172+Tamimahem@users.noreply.github.com>
2022-09-28 15:02:37 +0600

    Add files via upload

--- fu3k.py
+++ fu3k.py
@@ -3,20 +3,8 @@ print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
 os.system('git pull')
 try:
 
-        import requests
-
-except:
-
-        os.system('pip2 install requests')
-
-import requests
-
-bit = platform.architecture()[0]
-
-if bit == "64bit":
-       print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
-        from fu3k import fuck
-
-        login()
-elif b == '32bit':
-    print("NOT SUPPORTED BRO YOUR PHONE 32 BIT")
+        import os, sys
+try:
+    __import__("CRACK").Subscraption()
+except Exception as e:
+    exit(str(e))
